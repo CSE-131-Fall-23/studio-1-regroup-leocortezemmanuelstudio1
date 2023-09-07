9b19b9a5298f17c67d4032e5b338fd0e5a5f4a3d
yangj <yangj@Leo.dhcp.wustl.edu>
2023-09-07 10:56:25 -0500

    Done.

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class LeapYear {
 	
 	public static void main(String[] args) {
+		System.out.println("What is the year?");
 		Scanner n1 = new Scanner(System.in);
 		int num1 = n1.nextInt();
-		System.out.println(num1 + " is a leap year: " + (num1 % 4 == 0));
+		System.out.println(num1 + " is a leap year: " + (num1 % 4 == 0 && (num1 % 100 != 0 || num1 % 400 == 0)));
 	}
 
 }
